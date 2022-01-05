b40f32c5656c6bf79f2ef7c723c50ec51b71574e
mohammadne <mohammadne@mail.ru>
2022-01-05 17:21:26 +0330

    feat: update nginx

--- 4_advanced/samples/3.nginx.sh
+++ 4_advanced/samples/3.nginx.sh
@@ -1,5 +1,6 @@
 #? create configMap (option 1)
-kubectl create configmap nginx-config --from-file=3.reverseproxy.conf
+kubectl create configmap nginx-config --from-file=reverseproxy.conf=3.reverseproxy.conf
+# default key name will be 3.reverseproxy.conf, so we override it to reverseproxy.conf
 
 #? create configMap (option 2)
 kubectl create -f 3.cm.yml
@@ -13,3 +14,6 @@ kubectl create -f 3.nginx.yml
 kubectl exec -it helloworld-nginx -c nginx -- bash
 > cat /etc/conf.d/reverseproxy.conf
 # you can see it forwards the traffic to localhost:3000 (nodejs app)
+
+kubectl port-forward service/helloworld-nginx-service 31001
+curl localhost:31001
